860d22a0b97653bc3fb7efbb6149c2b228f12ac1
MMaicon <71274801+MMaicon@users.noreply.github.com>
2025-03-27 09:46:11 -0300

    login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById("formlogin").addEventListener("submit", function (event)
     const senha = document.getElementById("senha").value;
     const erroMensagem = document.getElementById("msgerror");
 
-    if (email === "ra" && senha === "erra") {
+    if (email === "0220832321005" && senha === "5001232380220") {
         alert("Login executado com sucesso!");
         window.location.href = "dashboard.html";
     } else {
